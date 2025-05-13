83dda7553ce91a0467c0b684041a67e9e09b0242
Augusto Poloni <a_poloni@unoesc.edu.br>
2025-05-13 20:04:51 -0300

    13/05

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Biblioteca 2025</h1>
+      <button className='btn btn-primary'>Teste</button>
+    </>
+    
+  );
+}
+
+export default App;
