e188387f52c15012ca0e44269a3a8f43b247dda4
Augusto Poloni <a_poloni@unoesc.edu.br>
2025-05-27 19:29:03 -0300

    v2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Menu from './componentes/Menu';
-import Home from './paginas/Home';
 import FormCategoria from './paginas/FormCategoria';
+import Home from './paginas/Home';
 import ListaCategoria from './paginas/ListaCategoria';
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Menu/>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/categorias' element={<ListaCategoria/>}/>
-          <Route path='/categoria' element={<FormCategoria/>}/>
-          <Route path='/categoria/:id' element={<FormCategoria/>}/>
-          <Route path='*' element={<Home/>}/>
-        </Routes>
+        <Menu />
+
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/listacategoria' element={<ListaCategoria />} />
+            <Route path='/cadastrocategoria' element={<FormCategoria />} />
+            <Route path='/cadastrocategoria/:id' element={<FormCategoria />} />
+            <Route path='*' element={<Home />} />
+          </Routes>
+        </div>
+
+
+
+
       </BrowserRouter>
     </>
-    
   );
 }
 
