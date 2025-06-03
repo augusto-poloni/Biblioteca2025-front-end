29e3a7443835042115d3c32661584398e6125c84
Augusto Poloni <a_poloni@unoesc.edu.br>
2025-06-03 20:54:21 -0300

    Questão1 - Editora

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Menu from './componentes/Menu';
+import Home from './paginas/Home';
+
+function App() {
+  return (
+    <>
+      <Menu/>
+      <Home/>
+    </>
+    
+  );
+}
+
+export default App;
